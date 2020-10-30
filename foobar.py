b2db363ee7c322badd64b89113b65de34cffd321
Rwitabrata <66558745+Rwitabrata@users.noreply.github.com>
2020-10-31 01:19:23 +0530

    Add files via upload

---
+++ foobar.py
@@ -0,0 +1,14 @@
+num = int(input("Enter the number here" ))
+if num%5 == 0:
+    print("foo")
+else:
+    print("the number isn't divisible by 5")
+if num%3 == 0:
+    print("bar")
+else:
+    print("the number isn't divisible by 3")
+if num%15 == 0:
+    print("foobar")
+else:
+    print("the number isn't divisible by 15")
+
